6dad3aecf5374bea345d979636b0b749961f3882
gangbeomsu <kbs21c1987@gmail.com>
2024-06-16 16:37:46 +0900

    테스트 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    where: {
+      id: 1,
+    },
+  });
+
+  console.log(posts);
+
   return <main></main>;
 }
